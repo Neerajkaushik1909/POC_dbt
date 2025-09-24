62b8ff793b4ba267ba4f54a6df4c8ec6bcc29bb3
neeraj.kaushik-ext@dialpad.com <neeraj.kaushik-ext@dialpad.com>
2025-09-24 06:45:09 +0000

    final commit

--- models/staging/contracts_sfdc.sql
+++ models/staging/contracts_sfdc.sql
@@ -1,6 +1,4 @@
-
 {{ config(materialized='ephemeral') }}
-
     SELECT
         a.*,
         CASE
@@ -8,10 +6,6 @@
             ELSE TRUE
         END
         isValid
-        FROM (
-        SELECT
-            * REPLACE((
-            SELECT STRING_AGG(DISTINCT i) FROM c.contract_id i ) AS contract_id )
-        FROM  {{ref("sfdc_contract_acc_data")}}  c) a
+        FROM ( SELECT * REPLACE(( SELECT STRING_AGG(DISTINCT i) FROM c.contract_id i ) AS contract_id ) FROM  {{ref("sfdc_contract_acc_data")}}  c) a
         WHERE
         row_num = 1
