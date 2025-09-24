38fcf91b3586b2ad225b0d8b44fe3db03125b9de
neeraj.kaushik-ext@dialpad.com <neeraj.kaushik-ext@dialpad.com>
2025-09-24 05:05:46 +0000

    sept 24

--- models/example/ssa_cm.sql
+++ models/example/ssa_cm.sql
@@ -1,133 +1,230 @@
-
-SELECT
-  c.company_id AS csr_company_id,
-  CASE
-    WHEN c.is_reseller=TRUE THEN NULL
-    WHEN cp.account_id IS NOT NULL THEN cp.account_id
-    WHEN cs.account_id IS NOT NULL THEN cs.account_id
-    ELSE NULL
-END AS sfdc_account_id,
-  CASE
-    WHEN c.is_reseller=TRUE THEN NULL
-    WHEN cp.contract_id IS NOT NULL THEN cp.contract_id
-    WHEN cs.contract_id IS NOT NULL THEN cs.contract_id
-    ELSE NULL
-END
-  AS contract_id,
-  CASE
-    WHEN c.is_reseller=TRUE THEN NULL
-    WHEN CAST(cp.contract_number as STRING) IS NOT NULL THEN cp.contract_number
-    WHEN CAST(cs.contract_number as STRING) IS NOT NULL THEN CAST(cs.contract_number as STRING)
-    ELSE NULL
-END
-  AS contract_number,
-  CASE
-    WHEN c.is_reseller=TRUE THEN NULL
-    WHEN cp.account_name IS NOT NULL THEN cp.account_name
-    WHEN cs.account_name IS NOT NULL THEN cs.account_name
-    ELSE NULL
-END
-  AS account_name,
-  CASE
-    WHEN coc.company_id IS NOT NULL THEN coc.start_date
-    ELSE CASE
-    WHEN c.is_reseller=TRUE THEN CASE
-    WHEN c.plan_period='yearly' THEN
-  CASE
-    WHEN DATE_SUB(c.next_billing_date, INTERVAL 1 year)<= CURRENT_DATE() THEN DATE_SUB(c.next_billing_date, INTERVAL 1 year)
-    ELSE DATE_SUB(DATE_SUB(c.next_billing_date, INTERVAL 1 year), INTERVAL 14 day)
-END
-    WHEN c.plan_period='monthly' THEN CASE
-    WHEN DATE_SUB(c.next_billing_date, INTERVAL 1 month)<= CURRENT_DATE() THEN DATE_SUB(c.next_billing_date, INTERVAL 1 month)
-    ELSE DATE_SUB(DATE_SUB(c.next_billing_date, INTERVAL 1 month), INTERVAL 14 day)
-END
-    WHEN c.plan_period='quarterly' THEN CASE
-    WHEN DATE_SUB(c.next_billing_date, INTERVAL 3 month)<= CURRENT_DATE() THEN DATE_SUB(c.next_billing_date, INTERVAL 3 month)
-    ELSE DATE_SUB(DATE_SUB(c.next_billing_date, INTERVAL 3 month), INTERVAL 14 day)
-END
-    WHEN c.plan_period='semiannual' THEN CASE
-    WHEN DATE_SUB(c.next_billing_date, INTERVAL 6 month)<= CURRENT_DATE() THEN DATE_SUB(c.next_billing_date, INTERVAL 6 month)
-    ELSE DATE_SUB(DATE_SUB(c.next_billing_date, INTERVAL 6 month), INTERVAL 14 day)
-END
-END
-    WHEN cp.contract_start_date IS NOT NULL THEN cp.contract_start_date
-    WHEN cs.contract_start_date IS NOT NULL THEN cs.contract_start_date
-    WHEN c.under_contract =FALSE THEN CASE
-    WHEN c.plan_period='yearly' THEN
-  CASE
-    WHEN DATE_SUB(c.next_billing_date, INTERVAL 1 year)<= CURRENT_DATE() THEN DATE_SUB(c.next_billing_date, INTERVAL 1 year)
-    ELSE DATE_SUB(DATE_SUB(c.next_billing_date, INTERVAL 1 year), INTERVAL 14 day)
-END
-    WHEN c.plan_period='monthly' THEN CASE
-    WHEN DATE_SUB(c.next_billing_date, INTERVAL 1 month)<= CURRENT_DATE() THEN DATE_SUB(c.next_billing_date, INTERVAL 1 month)
-    ELSE DATE_SUB(DATE_SUB(c.next_billing_date, INTERVAL 1 month), INTERVAL 14 day)
-END
-    WHEN c.plan_period='quarterly' THEN CASE
-    WHEN DATE_SUB(c.next_billing_date, INTERVAL 3 month)<= CURRENT_DATE() THEN DATE_SUB(c.next_billing_date, INTERVAL 3 month)
-    ELSE DATE_SUB(DATE_SUB(c.next_billing_date, INTERVAL 3 month), INTERVAL 14 day)
-END
-    WHEN c.plan_period='semiannual' THEN CASE
-    WHEN DATE_SUB(c.next_billing_date, INTERVAL 6 month)<= CURRENT_DATE() THEN DATE_SUB(c.next_billing_date, INTERVAL 6 month)
-    ELSE DATE_SUB(DATE_SUB(c.next_billing_date, INTERVAL 6 month), INTERVAL 14 day)
-END
-END
-    ELSE NULL
-END
-END
-  AS start_date,
-  CASE
-    WHEN coc.company_id IS NOT NULL THEN coc.end_date
-    ELSE CASE
-    WHEN c.is_reseller=TRUE THEN NULL
-    WHEN cp.contract_end_date IS NOT NULL THEN cp.contract_end_date
-    WHEN cs.contract_end_date IS NOT NULL THEN cs.contract_end_date
-    WHEN c.under_contract =FALSE THEN NULL
-    ELSE NULL
-END
-END
-  AS end_date,
-  'Activated' AS contract_status,
-  CASE
-    WHEN coc.company_id IS NOT NULL AND coc.end_date IS NULL THEN TRUE
-    WHEN coc.company_id IS NOT NULL
-  AND coc.end_date IS NOT NULL THEN FALSE
-    WHEN c.is_reseller=TRUE THEN TRUE
-    WHEN cp.contract_start_date IS NOT NULL THEN FALSE
-    WHEN cs.contract_start_date IS NOT NULL THEN FALSE
-    WHEN c.under_contract =FALSE THEN TRUE
-    ELSE CAST(NULL AS boolean)
-END
-  AS is_evergreen_contract,
-  CASE
-    WHEN coc.company_id IS NOT NULL THEN 'override_customer'
-    WHEN c.is_reseller=TRUE THEN 'csr'
-    WHEN cp.contract_id IS NOT NULL THEN 'sfdc_pubsub'
-    WHEN cs.contract_id IS NOT NULL THEN 'sfdc'
-    WHEN c.under_contract =FALSE THEN 'csr'
-    ELSE NULL
-END
-  AS source_system,
-  current_timestamp AS date_created,
-  current_timestamp AS date_modified,
-  CAST(csr_latest_update_date as timestamp) as csr_latest_update_date,
-  CASE
-    WHEN cs.sfdc_last_updated_date IS NOT NULL THEN CAST(cp.sfdc_last_updated_date as timestamp)
-    WHEN cp.sfdc_last_updated_date IS NOT NULL THEN CAST(cs.sfdc_last_updated_date as timestamp)
-    ELSE NULL
-END
-  AS sfdc_last_updated_date
-FROM
-  {{ref("company_data")}} c
-LEFT JOIN (SELECT * FROM {{source("raw_netsuite","contract_override_customers_table")}} WHERE Is_override=TRUE) coc
-ON
-  c.company_id=CAST(coc.company_id AS string)
-LEFT JOIN
-  {{ref("contracts_pubsub")}} cp
-ON
-  c.company_id=cp.dialpad_company_id
-LEFT JOIN
-  {{ref("contracts_sfdc")}} cs --tested working fine.
-ON
-  c.company_id=cs.company_id
-WHERE
-  NOT(cp.contract_id IS NULL AND COALESCE(cs.totalrows,1)>1) 
+select
+    c.company_id as csr_company_id,
+    case
+        when c.is_reseller = true
+        then null
+        when cp.account_id is not null
+        then cp.account_id
+        when cs.account_id is not null
+        then cs.account_id
+        else null
+    end as sfdc_account_id,
+    case
+        when c.is_reseller = true
+        then null
+        when cp.contract_id is not null
+        then cp.contract_id
+        when cs.contract_id is not null
+        then cs.contract_id
+        else null
+    end as contract_id,
+    case
+        when c.is_reseller = true
+        then null
+        when cast(cp.contract_number as string) is not null
+        then cp.contract_number
+        when cast(cs.contract_number as string) is not null
+        then cast(cs.contract_number as string)
+        else null
+    end as contract_number,
+    case
+        when c.is_reseller = true
+        then null
+        when cp.account_name is not null
+        then cp.account_name
+        when cs.account_name is not null
+        then cs.account_name
+        else null
+    end as account_name,
+    case
+        when coc.company_id is not null
+        then coc.start_date
+        else
+            case
+                when c.is_reseller = true
+                then
+                    case
+                        when c.plan_period = 'yearly'
+                        then
+                            case
+                                when
+                                    date_sub(c.next_billing_date, interval 1 year)
+                                    <= current_date()
+                                then date_sub(c.next_billing_date, interval 1 year)
+                                else
+                                    date_sub(
+                                        date_sub(c.next_billing_date, interval 1 year),
+                                        interval 14 day
+                                    )
+                            end
+                        when c.plan_period = 'monthly'
+                        then
+                            case
+                                when
+                                    date_sub(c.next_billing_date, interval 1 month)
+                                    <= current_date()
+                                then date_sub(c.next_billing_date, interval 1 month)
+                                else
+                                    date_sub(
+                                        date_sub(c.next_billing_date, interval 1 month),
+                                        interval 14 day
+                                    )
+                            end
+                        when c.plan_period = 'quarterly'
+                        then
+                            case
+                                when
+                                    date_sub(c.next_billing_date, interval 3 month)
+                                    <= current_date()
+                                then date_sub(c.next_billing_date, interval 3 month)
+                                else
+                                    date_sub(
+                                        date_sub(c.next_billing_date, interval 3 month),
+                                        interval 14 day
+                                    )
+                            end
+                        when c.plan_period = 'semiannual'
+                        then
+                            case
+                                when
+                                    date_sub(c.next_billing_date, interval 6 month)
+                                    <= current_date()
+                                then date_sub(c.next_billing_date, interval 6 month)
+                                else
+                                    date_sub(
+                                        date_sub(c.next_billing_date, interval 6 month),
+                                        interval 14 day
+                                    )
+                            end
+                    end
+                when cp.contract_start_date is not null
+                then cp.contract_start_date
+                when cs.contract_start_date is not null
+                then cs.contract_start_date
+                when c.under_contract = false
+                then
+                    case
+                        when c.plan_period = 'yearly'
+                        then
+                            case
+                                when
+                                    date_sub(c.next_billing_date, interval 1 year)
+                                    <= current_date()
+                                then date_sub(c.next_billing_date, interval 1 year)
+                                else
+                                    date_sub(
+                                        date_sub(c.next_billing_date, interval 1 year),
+                                        interval 14 day
+                                    )
+                            end
+                        when c.plan_period = 'monthly'
+                        then
+                            case
+                                when
+                                    date_sub(c.next_billing_date, interval 1 month)
+                                    <= current_date()
+                                then date_sub(c.next_billing_date, interval 1 month)
+                                else
+                                    date_sub(
+                                        date_sub(c.next_billing_date, interval 1 month),
+                                        interval 14 day
+                                    )
+                            end
+                        when c.plan_period = 'quarterly'
+                        then
+                            case
+                                when
+                                    date_sub(c.next_billing_date, interval 3 month)
+                                    <= current_date()
+                                then date_sub(c.next_billing_date, interval 3 month)
+                                else
+                                    date_sub(
+                                        date_sub(c.next_billing_date, interval 3 month),
+                                        interval 14 day
+                                    )
+                            end
+                        when c.plan_period = 'semiannual'
+                        then
+                            case
+                                when
+                                    date_sub(c.next_billing_date, interval 6 month)
+                                    <= current_date()
+                                then date_sub(c.next_billing_date, interval 6 month)
+                                else
+                                    date_sub(
+                                        date_sub(c.next_billing_date, interval 6 month),
+                                        interval 14 day
+                                    )
+                            end
+                    end
+                else null
+            end
+    end as start_date,
+    case
+        when coc.company_id is not null
+        then coc.end_date
+        else
+            case
+                when c.is_reseller = true
+                then null
+                when cp.contract_end_date is not null
+                then cp.contract_end_date
+                when cs.contract_end_date is not null
+                then cs.contract_end_date
+                when c.under_contract = false
+                then null
+                else null
+            end
+    end as end_date,
+    'Activated' as contract_status,
+    case
+        when coc.company_id is not null and coc.end_date is null
+        then true
+        when coc.company_id is not null and coc.end_date is not null
+        then false
+        when c.is_reseller = true
+        then true
+        when cp.contract_start_date is not null
+        then false
+        when cs.contract_start_date is not null
+        then false
+        when c.under_contract = false
+        then true
+        else cast(null as boolean)
+    end as is_evergreen_contract,
+    case
+        when coc.company_id is not null
+        then 'override_customer'
+        when c.is_reseller = true
+        then 'csr'
+        when cp.contract_id is not null
+        then 'sfdc_pubsub'
+        when cs.contract_id is not null
+        then 'sfdc'
+        when c.under_contract = false
+        then 'csr'
+        else null
+    end as source_system,
+    current_timestamp as date_created,
+    current_timestamp as date_modified,
+    cast(csr_latest_update_date as timestamp) as csr_latest_update_date,
+    case
+        when cs.sfdc_last_updated_date is not null
+        then cast(cp.sfdc_last_updated_date as timestamp)
+        when cp.sfdc_last_updated_date is not null
+        then cast(cs.sfdc_last_updated_date as timestamp)
+        else null
+    end as sfdc_last_updated_date
+from {{ ref("company_data") }} c
+left join
+    (
+        select *
+        from {{ source("raw_netsuite", "contract_override_customers_table") }}
+        where is_override = true
+    ) coc
+    on c.company_id = cast(coc.company_id as string)
+left join {{ ref("contracts_pubsub") }} cp on c.company_id = cp.dialpad_company_id
+left join
+    {{ ref("contracts_sfdc") }} cs  -- tested working fine.
+    on c.company_id = cs.company_id
+where not (cp.contract_id is null and coalesce(cs.totalrows, 1) > 1)
